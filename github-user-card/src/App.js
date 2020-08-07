ceb0b18565d6e9b5a9b56c0a2038facbe53a01c7
mikecrbenton <mikecrbenton@hotmail.com>
2020-08-06 21:29:15 -0500

    push to branch

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -2,33 +2,99 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import DisplayCard from './components/DisplayCard';
+import styled from 'styled-components';
 
 class App extends React.Component {
   constructor(){
      super();
      this.state = {
         card: [],
-        followers: []
+        followers: [],
+        searchText: ""
      }
   }
-
+  // ************componentDidMount()*******************
+  // AFTER INITIAL RENDER- RUNS ONCE ONLY
+  // CDMount - USE TO MAKE INITIAL FETCH/ API CALL
+  // SET STATE
   componentDidMount() {
-     axios.get('https://api.github.com/users/mikecrbenton')
-      //.then( (result) => result.json())
-      .then( (card) => { console.log(card.data); this.setState({ card : card.data })} )
+     axios.get('https://api.github.com/users/tetondan')
+      .then( (card) => { this.setState({ card : card.data })} )
       .catch( (error) => console.log("Error: ", error))
-      console.log(this.state.card)
+
+      axios.get('https://api.github.com/users/tetondan/followers')
+      .then( (followers) => { this.setState({ followers : followers.data })} )
+      .catch( (error) => console.log("Error: ", error))
   }
+
+  handleChange = (e) => {
+   this.setState(
+      { ...this.state, // <--- YOU ALMOST FORGOT THIS STEP - ASK VINCE ABOUT
+         searchText: e.target.value 
+      });
+   //would dynamic be [e.target.name] : e.target.value ?
+  }
+
+  /* // ALWAYS TAKES THESE 2 ARGS
+  componentDidUpdate(prevProps, prevState){
+     //checks for changing state -> lets us do something with the data that's changed
+     if(this.state.keyonobject !== prevState.keyonobject){
+         console.log()
+     }
+     console.log("State Updated", this.state.--------)
+  } */
+
+  getUser = (e) => {
+   e.preventDefault();
+
+   axios.get(`https://api.github.com/users/${this.state.searchText}`)
+      .then( (card) => this.setState({card: card.data}))
+      .catch( (error) => console.log("Error: ", error))
+   
+      //possibly put this in CDU ?
+   axios.get(`https://api.github.com/users/${this.state.searchText}/followers`)
+      .then( (followers) => { this.setState({ followers : followers.data })} )
+      .catch( (error) => console.log("Error: ", error))
+  };
   
   render(){
       return (
-         <div className="App">
-            <h1>GitHub User Cards</h1>
-            <DisplayCard card={this.state.card}/>
-         </div>
+         <MainContainer className="App">
+            <h1 className="page-header">GitHub User Cards</h1>
+            <input
+               type="text"
+               value={this.state.searchText}
+               onChange={this.handleChange}
+            />
+            <button onClick={this.getUser}>Git Get</button>
+            <DisplayCard card={this.state.card} followers={this.state.followers}/>
+         </MainContainer>
       );
   }
 
 }
 
 export default App;
+
+const MainContainer = styled.div`
+   background-color: #F8F8F8;
+   padding: 1em;
+
+   .page-header{
+      margin: 0;
+      font-size: 3rem;
+      text-shadow: 1px 1px 3px gray;
+   }
+
+   input{
+      border: 4px solid #009B77;
+      border-radius: 5px;
+      padding: .5em;
+      margin-right: 1em;
+   }
+   button{
+      border: 4px solid #009B77;
+      border-radius: 5px;
+      padding: .5em 1em;
+   }
+`;
--- github-user-card/src/components/DisplayCard.js
+++ github-user-card/src/components/DisplayCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
+// COULD HAVE USED A FUNCTION COMPONENT
 class DisplayCard extends React.Component {
    //DONT NEED IN A DISPLAY ELEMENT
    // constructor() {
@@ -11,10 +11,27 @@ class DisplayCard extends React.Component {
    render() {
       return (
          <GitCard>
-         <p>{this.props.card.name}</p>
-            <img src={this.props.card.avatar_url}/>
-            <p>{this.props.card.bio}</p>
-            <p>{this.props.card.repos_url}</p>
+
+            <div className="card-header">
+               <h1>{this.props.card.name}</h1>
+               <img src={this.props.card.avatar_url}/>
+            </div>
+
+            <p className="git-bio">{this.props.card.bio}</p>
+            <a href={this.props.card.avatar_url}>{this.props.card.repos_url}</a>
+            
+            <h3>Followers:</h3>
+            <hr></hr>
+            <div className="follower-container">
+               {this.props.followers.map( (follower) => {
+                  return <Follower>
+                           {console.log(follower)}
+                           <p>{follower.login}</p>
+                           <img src={follower.avatar_url}/>
+                        </Follower>
+               })}
+            </div>
+
          </GitCard>
 
       )
@@ -25,5 +42,64 @@ export default DisplayCard;
 
 
 const GitCard = styled.div`
+   background-color: #FFF;
+   border: 6px solid #009B77;
+   box-shadow: 0px 0px 5px black; 
+   border-radius: 10px;
+   width: 50%;
+   margin: 1em auto 0;
 
+   .card-header{
+      display: flex;
+      justify-content: center;
+      padding-top: 1em;
+   }
+
+   img{
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      margin-left: 1em;
+      box-shadow: 1px 1px 5px black;
+   }
+
+   .git-bio{
+      font-weight: 700;
+      font-size: 1.2rem;
+   }
+
+   a{
+      color: #009B77;
+      font-weight: 700;
+   }
+
+   hr{
+      width: 80%;
+   }
+
+   .follower-container{
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-evenly;
+   }
+`;
+
+const Follower = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   width: 30%;
+   margin-bottom: 1em;
+   padding: 0 0 1em;
+
+   p{
+      font-weight: 700;
+   }
+
+   img{
+      width: 80px;
+      height: 80px;
+      margin: 0;
+   }
 `;
